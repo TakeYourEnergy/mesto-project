0d8f80d1b5adee54f18ddea807faea023ce3aa5f
Alexander <sashashvetsov3@gmail.com>
2021-12-27 21:50:55 +0300

    Доработать попап на закрытие после ввода данных + в инпуты должны попадать значения из profileName and profileText

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,46 @@
-let buttonEdit = document.querySelector('.profile__button-edit')
-let popup = document.querySelector('.popup')
-let buttonClose = document.querySelector('.popup__button-close')
+const buttonEdit = document.querySelector('.profile__button-edit')
+const popup = document.querySelector('.popup')
+const buttonClose = document.querySelector('.popup__button-close')
+const formElement = document.querySelector('.popup__information') // форма
+const nameInput = document.querySelector('.popup__input-name')
+const jobInput = document.querySelector('.popup__input-about')
+const profileName = document.querySelector('.profile__name')
+const profileText = document.querySelector('.profile__text')
 
-//открытие popup
+
+
+//!открытие popup
 buttonEdit.addEventListener('click', () => {
   popup.classList.add('popup_opened')
 })
 
-//закрытие popup
+//!закрытие popup - по крестику
 buttonClose.addEventListener('click', () => {
   popup.classList.remove('popup_opened')
 })
+
+//!закрытие popup - по клику на фон (вне формы)
+popup.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    popup.classList.remove('popup_opened')
+  }
+})
+
+//!закрытие popup - по клавише Esc
+document.addEventListener('keydown', (e) => {
+  //console.log(e.code)
+  if (e.code === "Escape") {
+    popup.classList.remove('popup_opened')
+  }
+})
+
+//!форма по яндексу
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault()
+  profileName.textContent = nameInput.value
+  profileText.textContent = jobInput.value
+})
+
+//console.log(popup.classList.contains('popup_opened'));
+
 
